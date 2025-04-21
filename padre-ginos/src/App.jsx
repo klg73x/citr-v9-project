2bbc4aee93fd27f501122b72c83bd290279d7574
Kevin Gagnon <keving@kultera.com>
2025-04-21 17:29:07 -0400

    Added the Cart

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Padre Gino's Pizza - Order Now</h1>
+        <h1 className="logo">Padre Gino's Pizza - Order Now</h1>
         <Order />
+        <PizzaOfTheDay/>
       </div>
     </StrictMode>
   );
